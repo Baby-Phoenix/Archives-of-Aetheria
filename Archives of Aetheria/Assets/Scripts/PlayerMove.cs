2a7317375e73d0081e119f66763e54836c6f3d0d
Ayi <pranayanda@gmail.com>
2022-04-01 16:58:16 -0400

    Player Attack
    - 3 hit combo

--- Archives of Aetheria/Assets/Scripts/PlayerMove.cs
+++ Archives of Aetheria/Assets/Scripts/PlayerMove.cs
@@ -26,8 +26,11 @@ public class PlayerMove : MonoBehaviour
     private float? jumpStartTime;
     private bool isJumping;
     private bool isGrounded;
+    private bool isAttacking;
     private Vector3 velocity;
     private Vector3 moveDir;
+
+    int comboCount;
 
 
     // Start is called before the first frame update
@@ -44,18 +47,91 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!isDodging) PlayerMovement();
+        if(!isDodging && !isAttacking) PlayerMovement();
 
-
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.LeftControl) && !isAttacking)
         {
             if (moveDir.magnitude != 0 && !isJumping)
             {
                 StartCoroutine(Dodge());
             }
         }
+        if (Input.GetMouseButtonDown(0))
+        {
+            AttackCombo();
+        }
     }
 
+    void AttackCombo()
+    {
+        if(comboCount == 0)
+        {
+            isAttacking = true;
+
+            animator.SetBool("isCombo1", true);
+
+            animator.SetBool("isCombo2", false);
+            animator.SetBool("isCombo3", false);
+
+            animator.SetBool("isComboDone", false);
+
+            comboCount = 1;
+            return;
+        }
+        if (comboCount != 0)
+        {
+            if(animator.GetBool("isComboPossible"))
+            {
+                isAttacking = true;
+
+                animator.SetBool("isComboPossible", false);
+                comboCount++;
+            }
+        }
+    }
+
+    public void ComboPossible()
+    {
+        animator.SetBool("isComboPossible", true);
+    }
+
+    public void Combo()
+    {
+        if (comboCount == 2)
+        {
+            animator.SetBool("isCombo2", true);
+
+            animator.SetBool("isCombo1", false);
+            animator.SetBool("isCombo3", false);
+
+            animator.SetBool("isComboDone", false);
+        }
+
+        else if (comboCount == 3)
+        {
+            animator.SetBool("isCombo3", true);
+
+            animator.SetBool("isCombo1", false);
+            animator.SetBool("isCombo2", false);
+
+            animator.SetBool("isComboDone", false);
+        }
+    }
+
+    public void ComboReset()
+    {
+        isAttacking = false;
+
+        animator.SetBool("isCombo1", false);
+        animator.SetBool("isCombo2", false);
+        animator.SetBool("isCombo3", false);
+        animator.SetBool("isComboPossible", false);
+
+        animator.SetBool("isComboDone", true);
+
+        comboCount = 0;
+    }
+    
     private void PlayerMovement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -121,7 +197,7 @@ public class PlayerMove : MonoBehaviour
                 characterController.height = 2.9f;
             }
         }
-        Debug.Log(ySpeed);
+
         if (moveDir != Vector3.zero)
         {
             animator.SetBool("isMoving", true);
