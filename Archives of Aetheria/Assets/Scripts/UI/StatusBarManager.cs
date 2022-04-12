4163bcb634746a28550d21a6a7caca2ce8893c27
Mrinank <mrinank.sivakumar@ontariotechu.net>
2022-04-12 16:14:40 -0400

    Merge Cleanup

--- Archives of Aetheria/Assets/Scripts/UI/StatusBarManager.cs
+++ Archives of Aetheria/Assets/Scripts/UI/StatusBarManager.cs
@@ -5,30 +5,34 @@ using UnityEngine.UI;
 
 public class StatusBarManager : MonoBehaviour
 {
-    public float health, maxHealth;
+    public float unit, maxUnit;
     private Slider slider;
     [SerializeField] private Image fill;
     [SerializeField] private Gradient gradient;
     [SerializeField] private bool isAbove = false;
+    public Text number;
 
 
     private void Awake()
     {
         slider = gameObject.GetComponent<Slider>();
+        number = gameObject.GetComponentInChildren<Text>();
     }
 
-    public void SetMaxHealth(float max)
+    public void SetMaxValue(float max)
     {
-        health = max;
-        maxHealth = max;
+        unit = max;
+        maxUnit = max;
         fill.color = gradient.Evaluate(1f);
+        number.text = unit.ToString() + "/" + maxUnit.ToString();
     }
 
-    public void UpdateHealth()
+    public void UpdateValue()
     {
-        float value = Mathf.Clamp(health / maxHealth, 0, 1f);
+        float value = Mathf.Clamp(unit / maxUnit, 0, 1f);
         slider.value = value;
         fill.color = gradient.Evaluate(value);
+        number.text = unit.ToString() + "/" + maxUnit.ToString();
     }
     private void LateUpdate()
     {
